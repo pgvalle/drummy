bd45099a6a0fce9d105acbab9f2a6f14e071fca9
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-29 16:37:40 -0300

    rearraning project structure

---
+++ drummix/src/sokol.cpp
@@ -0,0 +1,16 @@
+// #define NK_IMPLEMENTATION
+// #define NK_INCLUDE_FIXED_TYPES
+// #define NK_INCLUDE_DEFAULT_ALLOCATOR
+// #define NK_INCLUDE_STANDARD_IO
+// #define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+// #define NK_INCLUDE_FONT_BAKING
+// #define NK_INCLUDE_DEFAULT_FONT
+// #include "nuklear.h"
+
+// #define SOKOL_GLCORE
+#define SOKOL_NOAPI
+#define SOKOL_APP_IMPL
+// #define SOKOL_NUKLEAR_IMPL
+#include "sokol/sokol_app.h"
+// #include "sokol/sokol_gfx.h"
+// #include "sokol/sokol_nuklear.h"
