d315668f646bccae2bce699af21c90623d32c9ee
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-30 09:14:13 -0300

    using glfw and imgui

--- drumix/src/main.cpp
+++ drumix/src/main.cpp
@@ -1,24 +1,104 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstdint>
 #include <cassert>
 #include "serialib.h"
 
+GLFWwindow *window;
+ImGuiIO *imio;
 serialib serial;
 uint16_t pads[16];
 
-int main(int argc, char **argv) {
-    if (argc < 1) return 1;
-    if (serial.openDevice(argv[1], 115200) != 1) return 1;
-    
-    while (true) {
-        if (serial.available() < 32) continue;
-        serial.readBytes(pads, 32, 0, 0);
+void init();
+void quit();
+void beginFrame();
+void endFrame();
 
-        memset(pads, 0, sizeof(pads));
+int main() {
+    init();
+
+    while (!glfwWindowShouldClose(window)) {
+        beginFrame();
+
+        ImGui::Begin("Hello, world!", nullptr, ImGuiWindowFlags_NoTitleBar);
+        ImGui::Text("This is some useful text.");
+        ImGui::End();
+
+        endFrame();
     }
 
-    serial.closeDevice();
-
-    return 0;
+    quit();
 }
+
+void init() {
+    assert(glfwInit());
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(1280, 720, "drumix", nullptr, nullptr);
+    assert(window);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    imio = &ImGui::GetIO();
+    imio->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+}
+
+void quit() {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void beginFrame() {
+    glfwPollEvents();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    ImGui::SetNextWindowSize(imio->DisplaySize);
+    ImGui::SetNextWindowPos({0, 0});
+}
+
+void endFrame() {
+    ImGui::Render();
+    int w, h;
+    glfwGetFramebufferSize(window, &w, &h);
+    glViewport(0, 0, w, h);
+    glClearColor(0.7, 0.7, 0.7, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+}
+
+
+// int main(int argc, char **argv) {
+//     if (argc < 1) return 1;
+//     if (serial.openDevice(argv[1], 115200) != 1) return 1;
+    
+//     while (true) {
+//         if (serial.available() < 32) continue;
+//         serial.readBytes(pads, 32, 0, 0);
+
+//         memset(pads, 0, sizeof(pads));
+//     }
+
+//     serial.closeDevice();
+
+//     return 0;
+// }
