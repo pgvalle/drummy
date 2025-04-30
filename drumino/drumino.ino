acce93320e3435ce23ffd7ad0a3eb631bf2a705c
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-30 09:13:15 -0300

    update to arduino program

--- drumino/drumino.ino
+++ drumino/drumino.ino
@@ -1,7 +1,7 @@
 #define N_PADS 1
 #define MAX_PADS 16
-#define HIT_INTERVAL 20
 
+#define HIT_INTERVAL 20
 // pad states
 #define HIT     true
 #define NOT_HIT false
@@ -19,15 +19,14 @@ void setup() {
 
 void loop() {
   for (uint8_t i = 0; i < N_PADS; i++) {
-    uint16_t pinValue = analogRead(pins[i]);
-    uint32_t timeOnHit = millis() - timesLastHit[i];
-
-    if (states[i] == HIT && timeOnHit < HIT_INTERVAL) {
+    uint32_t timeHit = millis() - timesLastHit[i];
+    if (states[i] == HIT && timeHit < HIT_INTERVAL) {
       continue;
     }
-
+    
     states[i] = NOT_HIT;
-
+  
+    uint16_t pinValue = analogRead(pins[i]);
     if (pinValue >= 32) {
       states[i] = HIT;
       timesLastHit[i] = millis();
@@ -37,10 +36,9 @@ void loop() {
   }
 
   if (shouldSend) {
-    size_t lenBytes = MAX_PADS * sizeof(uint16_t);
-    Serial.write((char *)valuesToSend, lenBytes);
+    Serial.write((char *)valuesToSend, sizeof(valuesToSend));
     Serial.flush();
-    memset(valuesToSend, 0, lenBytes);
+    memset(valuesToSend, 0, sizeof(valuesToSend));
     shouldSend = false;
   }
 }
