2fc8a60162165884ff8069bfc1cc0744e4da66f3
pgvalle <pgvalle.nogueira@gmail.com>
2025-05-05 15:30:47 -0300

    compiling miniaudio in separate file and testing playback

--- drumix/src/daemon/main.cpp
+++ drumix/src/daemon/main.cpp
@@ -3,20 +3,26 @@
 #include <cstdint>
 #include <cassert>
 
-#define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
 #include <serialib.h>
 
 serialib serial;
 uint16_t pads[16];
+ma_engine engine;
 
 int main(int argc, char **argv) {
     if (argc < 1) return 1;
-    if (serial.openDevice(argv[1], 115200) != 1) return 1;
+    
+    if (serial.openDevice(argv[1], 115200) != 1) return 1; // serial
+    if (ma_engine_init(NULL, &engine) != MA_SUCCESS) return 1; // audio
     
     while (true) {
         if (serial.available() < 32) continue;
         serial.readBytes(pads, 32, 0, 0);
+
+        if (pads[0]) {
+            ma_engine_play_sound(&engine, "kick.wav", NULL);
+        }
 
         memset(pads, 0, sizeof(pads));
     }
